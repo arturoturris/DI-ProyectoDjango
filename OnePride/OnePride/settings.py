daaeea411619ae5b9dea2a792f8d7d4b125bff4f
Arturo Tenorio <arturo-turris@hotmail.com>
2020-10-15 18:11:46 -0500

    V0.1 - Directorios corregidos. Plantilla de servicios.

--- OnePride/OnePride/settings.py
+++ OnePride/OnePride/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PaginaWeb',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'OnePride.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./PaginaWeb/plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
