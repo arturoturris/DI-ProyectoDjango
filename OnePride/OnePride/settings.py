5c298b8cac34da23b1a518bc307a9c3d36f0eb49
MariaJoseFuentes <mjfp1506@gmail.com>
2020-10-15 19:43:12 -0500

    settings

--- OnePride/OnePride/settings.py
+++ OnePride/OnePride/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'OnePride.PaginaWeb'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
