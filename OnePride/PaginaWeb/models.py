82a03f3901fad9fc8cf655b4adfe5832536e9fec
MariaJoseFuentes <mjfp1506@gmail.com>
2020-10-15 20:29:40 -0500

    correciones

--- OnePride/PaginaWeb/models.py
+++ OnePride/PaginaWeb/models.py
@@ -5,4 +5,4 @@ class Servicio(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     nombre = models.CharField(max_length=15)
     descripción = models.CharField(max_length=200)
-    costo = models.PositiveIntegerField(max_lenght=10)
+    costo = models.PositiveIntegerField()
