cab56d23226e11ae65ac2b27b3c99b1c467ce401
aleelfg <53869345+aleelfg@users.noreply.github.com>
2020-10-15 23:04:26 -0500

    Update views.py

--- OnePride/PaginaWeb/views.py
+++ OnePride/PaginaWeb/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 
 def listarServicios(request):
     return render(request, 'lista.html')
+
+def form_view(request):
+    data ={
+        'form':ServicioForm
+    }
+    if request.method == 'POST':
+        form = ServicioForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    return render(request, 'servicio_form.html',data)
