96fd14bf09ed35b4e516f897eec263446eca3be7
MariaJoseFuentes <mjfp1506@gmail.com>
2020-10-15 19:14:17 -0500

    modelos

--- PaginaWeb/models.py
+++ PaginaWeb/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Servicio(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    nombre = models.CharField(max_length=15)
+    descripción = models.CharField(max_length=200)
+    costo = models.PositiveIntegerField(max_lenght=10)
